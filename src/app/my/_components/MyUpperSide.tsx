90383f7f081f8d017640964c5cabda2a6327696c
oneju <wonju.noh.24@gmail.com>
2024-05-10 02:00:13 +0900

    Test: use Recoil for get userInfo

--- src/app/my/_components/MyUpperSide.tsx
+++ src/app/my/_components/MyUpperSide.tsx
@@ -1,11 +1,19 @@
+'use client';
 import Link from 'next/link';
 import MyUserCard from './MyUserCard';
-const username = 'gildong';
-const area = '00시 00동';
+import { useRecoilValue } from 'recoil';
+import { useRouter } from 'next/navigation';
+import { userState } from '@/recoils/atoms/UserState';
+
 const MyUpperSide = () => {
+  const router = useRouter();
+  const { uid, username, address, storeInfo } = useRecoilValue(userState);
+
+  if (storeInfo) router.push(`/profile/${uid}`, { scroll: false });
+
   return (
     <div className="flex w-full p-10">
-      <MyUserCard username={username} address={area} />
+      <MyUserCard username={username} address={address} />
       <button className="h-10 w-fit flex-initial rounded-lg bg-gray-300 font-semibold text-white hover:bg-gray-500 focus-visible:outline-offset-2">
         <Link className="p-3 text-sm" href="/my/edit">
           내 정보 수정
